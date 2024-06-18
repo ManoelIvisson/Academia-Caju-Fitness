743013b8dd3d6e0ec890d08afd9b1c6e57334e97
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-06-18 16:38:35 -0300

    Subindo arquivos para o repositorio remoto

---
+++ src/main/java/org/academia_caju_fitness/AcademiaCajuFitnessApplication.java
@@ -0,0 +1,13 @@
+package org.academia_caju_fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaCajuFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaCajuFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/academia_caju_fitness/AcademiaCajuFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.academia_caju_fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaCajuFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
